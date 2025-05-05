3a379105a0a8dd4286e408abeecae645ffcd1e2c
Alexandr Ragel <ragel.alexandr@gmail.com>
2025-05-05 09:20:13 +0300

    вывод сообщений в форме авторизации

--- app/config.py
+++ app/config.py
@@ -2,3 +2,10 @@
 # Path: my_flask_app/app/config.py
 
 # Конфигурация (SECRET_KEY, параметры БД, настройки Flask-Mail и др.)
+
+# config.py
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'сложная_и_уникальная_строка'
+    # Другие настройки ...
