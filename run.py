7aa9d78decbb44e71d210ea340be1224495ac358
Alexandr Ragel <ragel.alexandr@gmail.com>
2025-05-05 08:53:17 +0300

    исправления запуска и главной страницы

--- run.py
+++ run.py
@@ -1,17 +1,14 @@
 # File: run.py
 # Path: my_flask_app/run.py
 
-# Точка входа для запуска приложения
-
 from app import create_app
 from flask import render_template
 
 app = create_app()
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html', title='Главная страница')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
